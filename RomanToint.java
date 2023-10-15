3685ae54faeb7bc2bf2e02476948907161fe3e19
PalasaAkhila <147700491+PalasaAkhila@users.noreply.github.com>
2023-10-15 20:45:26 +0530

    Add files via upload

---
+++ RomanToint.java
@@ -0,0 +1,59 @@
+
+package codingassigment;
+
+import java.util.Scanner;
+
+public class RomanToint{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a Roman numeral: ");
+        String romanNumeral = scanner.nextLine().toUpperCase();
+        scanner.close();
+
+        int result = 0;
+        int prevValue = 0;
+
+        for (int i = romanNumeral.length() - 1; i >= 0; i--) {
+            char currentChar = romanNumeral.charAt(i);
+            int currentValue;
+
+            switch (currentChar) {
+                case 'I':
+                    currentValue = 1;
+                    break;
+                case 'V':
+                    currentValue = 5;
+                    break;
+                case 'X':
+                    currentValue = 10;
+                    break;
+                case 'L':
+                    currentValue = 50;
+                    break;
+                case 'C':
+                    currentValue = 100;
+                    break;
+                case 'D':
+                    currentValue = 500;
+                    break;
+                case 'M':
+                    currentValue = 1000;
+                    break;
+                default:
+                    System.out.println("Invalid Roman numeral character.");
+                    return;
+            }
+
+            if (currentValue < prevValue) {
+                result -= currentValue;
+            } else {
+                result += currentValue;
+            }
+
+            prevValue = currentValue;
+        }
+
+        System.out.println("The integer value of " + romanNumeral + " is: " + result);
+    }
+}
+
